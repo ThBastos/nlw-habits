727aa2a294bffe7aa26cd5bc79560326a1338b9c
Thalles Bastos <thbastos.web@gmail.com>
2023-04-05 13:32:37 -0300

    Finalizado inicio server e começando web

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Habit } from "./components/Habit";
+
+function App() {
+  return (
+    <div>
+      <Habit />
+      <Habit />
+      <Habit />
+    </div>
+  );
+}
+
+export default App;
